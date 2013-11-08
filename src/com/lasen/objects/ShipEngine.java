b79f7ffd5b010a6fc17beeb6c95905d6fa242c2d
Lasenist <peterarmstrong518@gmail.com>
2013-11-08 10:28:51 +0000

    Create ShipEngine.java

---
+++ src/com/lasen/objects/ShipEngine.java
@@ -0,0 +1,22 @@
+package com.lasen.objects;
+
+
+/**
+ *
+ * @author Lasen
+ */
+public class ShipEngine extends ShipPart
+{
+  
+  private int enginePower;
+  
+  public ShipBattery(String name, int cost, int size, String ref, int acceleration, float x, float y, float yVelocity)
+  {
+    super(name,cost,size,ref, x, y, yVelocity);
+    this.enginePower = enginePower;
+  }
+  
+  
+  
+  
+}
