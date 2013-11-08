27b1f4c160ac1acdb1f396a6660afb10cd901736
Lasenist <peterarmstrong518@gmail.com>
2013-11-08 10:30:20 +0000

    Update ShipEngine.java

--- src/com/lasen/objects/ShipEngine.java
+++ src/com/lasen/objects/ShipEngine.java
@@ -9,6 +9,8 @@ public class ShipEngine extends ShipPart
 {
   
   private int enginePower;
+  private shipPart parent;
+  private shipPart[] children;
   
   public ShipBattery(String name, int cost, int size, String ref, int acceleration, float x, float y, float yVelocity)
   {
